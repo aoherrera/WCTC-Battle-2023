3ab04defd48ac2700cd705864db4d7738704f158
Alex Herrera <ao.herrera@outlook.com>
2023-09-21 20:14:06 -0500

    Extended implementations of IWeapon and IArmor.

---
+++ Models/Chaimmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Class05.Interfaces;
+
+namespace Class05.Models
+{
+	public class Chainmail : IArmor
+    {
+		public string Name { get; set; }
+		public int Defense { get; set; }
+
+        public Chainmail(string name, int defense)
+        {
+            Name = name;
+            Defense = defense;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace Class05
     {
         static void Main(string[] args)
         {
+            IArmor mediumArmor = new Chainmail("Black Chainmail", 20);
+            IWeapon mediumWeapon = new Hammer("Goblin Hammer", 50);
+
             IArmor armor = new Leather("Elven Leather Armor", 12);
             IWeapon sword = new Sword("Long sword", 5);
 
             ICombatant fighter = new Fighter("Fighter Fred", armor, sword);
             ICombatant badGuy = new Fighter("Scary Monster", armor, sword);
+            ICombatant bossEnemy = new Fighter("Scarier Monster", mediumArmor, mediumWeapon);
 
             fighter.Attack(badGuy);
 
